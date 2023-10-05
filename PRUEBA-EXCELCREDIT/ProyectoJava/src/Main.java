299784b8b2618cd1e61a7f2a26d2b39ce2755f04
secaja <secaja01@gmail.com>
2023-10-05 08:25:16 -0500

    commitc
    commit

--- PRUEBA-EXCELCREDIT/ProyectoJava/src/Main.java
+++ PRUEBA-EXCELCREDIT/ProyectoJava/src/Main.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int numero = 5;
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+
+        while (true) {
+            System.out.print("Ingrese un número para calcular su factorial: ");
+
+            if (scanner.hasNextInt()) {
+                numero = scanner.nextInt();
+                break; // Sale del bucle si la entrada es un número válido.
+            } else {
+                System.out.println("Entrada no válida. Por favor, ingrese un número válido.");
+                scanner.nextLine(); // Limpia la entrada incorrecta antes de continuar.
+            }
+        }
+
         long factorial = calcularFactorial(numero);
         System.out.println("El factorial de " + numero + " es " + factorial);
 
+        scanner.close(); // Cierra el scanner cuando ya no se necesita.
     }
+
     public static long calcularFactorial(int n) {
         long factorial = 1;
         for (int i = 1; i <= n; i++) {
--- PRUEBA-EXCELCREDIT/ProyectoJavaCadenas/src/Main.java
+++ PRUEBA-EXCELCREDIT/ProyectoJavaCadenas/src/Main.java
@@ -1,26 +1,64 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(contarVocalesConsonantes("Hola mi nombre es Sebastian Cadavid y soy desarrollador junior"));
+        // Crear un objeto Scanner para leer la entrada del usuario desde la consola.
+        Scanner scanner = new Scanner(System.in);
 
+        // Solicitar al usuario que ingrese una cadena de texto.
+        System.out.print("Ingrese una cadena de texto: ");
+        String input = scanner.nextLine(); // Leer la cadena ingresada por el usuario.
+
+        try {
+            // Llamar a la función contarVocalesConsonantes con la cadena de entrada y obtener el resultado.
+            Map<String, Integer> resultado = contarVocalesConsonantes(input);
+
+            // Mostrar el número de vocales y consonantes en la cadena.
+            System.out.println("Número de vocales: " + resultado.get("vocales"));
+            System.out.println("Número de consonantes: " + resultado.get("consonantes"));
+        } catch (InputVacioException e) {
+            // Manejar la excepción si la cadena de entrada está vacía.
+            System.out.println("Error: La cadena de entrada está vacía.");
+        }
+
+        // Cerrar el objeto Scanner para liberar recursos.
+        scanner.close();
     }
-    public static Map<String, Integer> contarVocalesConsonantes(String cadena) {
+
+    public static Map<String, Integer> contarVocalesConsonantes(String cadena) throws InputVacioException {
+        if (cadena.isEmpty()) {
+            // Lanzar una excepción personalizada si la cadena de entrada está vacía.
+            throw new InputVacioException("La cadena de entrada está vacía.");
+        }
+
+        // Inicializar map para contar el número de vocales y consonantes.
         Map<String, Integer> contadores = new HashMap<>();
         contadores.put("vocales", 0);
         contadores.put("consonantes", 0);
 
+        // Convertir la cadena a minúsculas para facilitar la comparación.
         String cadenaMinuscula = cadena.toLowerCase();
 
+        // Iterar a través de cada carácter en la cadena.
         for (char letra : cadenaMinuscula.toCharArray()) {
             if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {
+                // Incrementar el contador de vocales si el carácter es una vocal.
                 contadores.put("vocales", contadores.get("vocales") + 1);
             } else if (letra != ' ') {
+                // Incrementar el contador de consonantes si el carácter no es un espacio.
                 contadores.put("consonantes", contadores.get("consonantes") + 1);
             }
         }
 
+        // Devolver el map con los contadores de vocales y consonantes.
         return contadores;
+    }
+}
+
+class InputVacioException extends Exception {
+    public InputVacioException(String mensaje) {
+        super(mensaje);
     }
 }
